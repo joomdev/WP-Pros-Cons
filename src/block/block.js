4bddf97c84b9f4fde6298593902f223e464cbf24
actuallyakash <akash_gupta@hotmail.com>
2019-05-03 13:42:18 +0530

    Added editable lists

--- src/block/block.js
+++ src/block/block.js
@@ -1,12 +1,13 @@
 //  Import CSS.
 import './style.scss';
 import './editor.scss';
-
+import { RawHTML } from '@wordpress/element';
 
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
 const { Button } = wp.components;
 const { RichText, PlainText } = wp.editor;
+const { Fragment } = wp.element;
 
 registerBlockType( 'tc/block-prosandcons', {
 	title: __( 'Pros And Cons' ),
@@ -21,11 +22,27 @@ registerBlockType( 'tc/block-prosandcons', {
 	attributes: {
 		title: {
 			source: 'text',
-			selector: '.wpc-title'
+			selector: 'h3.wpc-title'
+		},
+		prosValues: {
+			type: "string",
+			source: "html",
+			selector: "ol,ul",
+			multiline: "li",
+			default: ""
+		},
+		consValues: {
+			type: "string",
+			source: "html",
+			selector: "ol,ul",
+			multiline: "li",
+			default: ""
 		}
 	},
 
-	edit({attributes, className, setAttributes}) {
+	edit({attributes, setAttributes, onReplace, className}) {
+		const { prosValues, consValues } = attributes;
+
 		return (
 			<div className="wp-pros-cons">
 				<h3 className="wp-pros-cons-title">
@@ -47,10 +64,19 @@ registerBlockType( 'tc/block-prosandcons', {
 							<div className="section-title">Pros</div>
 							{/* Here comes all the pros */}
 							<ul class="wpc_pros_list">
-								<li class="wpc_pro_single">Lorem</li>
-								<li class="wpc_pro_single">Ipsum</li>
-								<li class="wpc_pro_single">Dolor</li>
+								<RichText
+									identifier="prosValues"
+									multiline="li"
+									onChange={ ( nextValues ) => setAttributes( { prosValues: nextValues } ) }
+									value={ prosValues }
+									wrapperClassName="wpc_pros_list"
+									className="wpc_pro_single"
+									placeholder={ __( 'Write list…' ) }
+									onRemove={ () => onReplace( [] ) }
+									placeholder="Pros goes here.."
+								/>
 							</ul>
+							
 						</div>
 					</div>
 					<div className="wp-pros-cons-col">
@@ -63,9 +89,16 @@ registerBlockType( 'tc/block-prosandcons', {
 							<div className="section-title">Cons</div>
 							{/* Here comes all the cons */}
 							<ul class="wpc_cons_list">
-								<li class="wpc_con_single">Dolor</li>
-								<li class="wpc_con_single">Ipsum</li>
-								<li class="wpc_con_single">Lorem</li>
+								<RichText
+									identifier="consValues"
+									multiline="li"
+									onChange={ ( nextValues ) => setAttributes( { consValues: nextValues } ) }
+									value={ consValues }
+									wrapperClassName="wpc_cons_list"
+									className="wpc_con_single"
+									onRemove={ () => onReplace( [] ) }
+									placeholder="Cons goes here.."
+								/>
 							</ul>
 						</div>
 					</div>
@@ -79,7 +112,7 @@ registerBlockType( 'tc/block-prosandcons', {
 		return (
 			<div className="wp-pros-cons">
 				<h3 className="wp-pros-cons-title wpc-title">
-					{ attributes.title }
+					<RawHTML>{ attributes.title }</RawHTML>
 				</h3>
 				<div className="wp-pros-cons-sections">
 					<div className="wp-pros-cons-col">
